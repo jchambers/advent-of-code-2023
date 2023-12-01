19c1ac5840bf2e99edcc29034b4aacaa26fd3547
Jon Chambers <jon.chambers@gmail.com>
2023-12-01 10:47:33 -0500

    Add a solution for day 1, part 2

--- src/bin/day01.rs
+++ src/bin/day01.rs
@@ -7,11 +7,21 @@ fn main() -> Result<(), Box<dyn Error>> {
     let args: Vec<String> = env::args().collect();
 
     if let Some(path) = args.get(1) {
-        let sum = calibration_sum(BufReader::new(File::open(path)?)
-            .lines()
-            .filter_map(|line| line.ok()));
+        {
+            let sum = calibration_sum(BufReader::new(File::open(path)?)
+                .lines()
+                .filter_map(|line| line.ok()));
 
-        println!("Sum of calibration values: {}", sum);
+            println!("Sum of calibration values: {}", sum);
+        }
+
+        {
+            let sum = calibration_sum_textual(BufReader::new(File::open(path)?)
+                .lines()
+                .filter_map(|line| line.ok()));
+
+            println!("Sum of calibration values with text interpretation: {}", sum);
+        }
 
         Ok(())
     } else {
@@ -23,8 +33,8 @@ fn calibration_value(line: &str) -> u32 {
     if let (Some(first), Some(last)) = (line.find(char::is_numeric), line.rfind(char::is_numeric)) {
         let bytes = line.as_bytes();
 
-        let first_digit = bytes[first] - '0' as u8;
-        let last_digit = bytes[last] - '0' as u8;
+        let first_digit = bytes[first] - b'0';
+        let last_digit = bytes[last] - b'0';
 
         first_digit as u32 * 10 + last_digit as u32
     } else {
@@ -34,6 +44,40 @@ fn calibration_value(line: &str) -> u32 {
 
 fn calibration_sum(lines: impl Iterator<Item = String>) -> u32 {
     lines.map(|line| calibration_value(line.as_str())).sum()
+}
+
+fn calibration_value_textual(line: &str) -> u32 {
+    let mut digits = Vec::new();
+
+    digits.extend(line.match_indices(char::is_numeric)
+        .map(|(index, slice)| (index, (slice.as_bytes()[0] - b'0') as u32)));
+
+    for (digit_string, digit_value) in [
+        ("one", 1),
+        ("two", 2),
+        ("three", 3),
+        ("four", 4),
+        ("five", 5),
+        ("six", 6),
+        ("seven", 7),
+        ("eight", 8),
+        ("nine", 9)] {
+
+        digits.extend(line.match_indices(digit_string)
+            .map(|(index, _)| (index, digit_value)));
+    }
+
+    digits.sort_by_key(|(index, _)| *index);
+
+    if let (Some((_, first)), Some((_, last))) = (digits.first(), digits.last()) {
+        first * 10 + last
+    } else {
+        0
+    }
+}
+
+fn calibration_sum_textual(lines: impl Iterator<Item = String>) -> u32 {
+    lines.map(|line| calibration_value_textual(line.as_str())).sum()
 }
 
 #[cfg(test)]
@@ -59,5 +103,31 @@ mod test {
         "};
 
         assert_eq!(142, calibration_sum(lines.lines().map(String::from)));
+    }
+
+    #[test]
+    fn test_calibration_value_textual() {
+        assert_eq!(29, calibration_value_textual("two1nine"));
+        assert_eq!(83, calibration_value_textual("eightwothree"));
+        assert_eq!(13, calibration_value_textual("abcone2threexyz"));
+        assert_eq!(24, calibration_value_textual("xtwone3four"));
+        assert_eq!(42, calibration_value_textual("4nineeightseven2"));
+        assert_eq!(14, calibration_value_textual("zoneight234"));
+        assert_eq!(76, calibration_value_textual("7pqrstsixteen"));
+    }
+
+    #[test]
+    fn test_calibration_sum_textual() {
+        let lines = indoc! {"
+            two1nine
+            eightwothree
+            abcone2threexyz
+            xtwone3four
+            4nineeightseven2
+            zoneight234
+            7pqrstsixteen
+        "};
+
+        assert_eq!(281, calibration_sum_textual(lines.lines().map(String::from)));
     }
 }
