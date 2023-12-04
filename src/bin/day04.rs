65dec28f505215d814c0f5077dfad2f2fc97ede4
Jon Chambers <jon.chambers@gmail.com>
2023-12-04 09:45:22 -0500

    Add a solution for day 4, part 2

--- src/bin/day04.rs
+++ src/bin/day04.rs
@@ -19,28 +19,47 @@ fn main() -> Result<(), Box<dyn Error>> {
             cards.iter().map(Card::score).sum::<u32>()
         );
 
+        println!("Total card count after expansion: {}", expand_cards(&cards));
+
         Ok(())
     } else {
         Err("Usage: day04 INPUT_FILE_PATH".into())
     }
 }
 
+fn expand_cards(cards: &[Card]) -> u32 {
+    // Assume that cards are serially numbered from 1 to N
+    let mut counts = vec![1; cards.len()];
+
+    for card in cards {
+        let matching_numbers = card.matching_numbers();
+        let copied_card_count = counts[(card.id - 1) as usize];
+
+        for copied_card_id in card.id..card.id + matching_numbers {
+            counts[copied_card_id as usize] += copied_card_count;
+        }
+    }
+
+    counts.iter().sum()
+}
+
 struct Card {
-    _id: u32,
+    id: u32,
 
     winning_numbers: Vec<u32>,
     drawn_numbers: Vec<u32>,
 }
 
 impl Card {
-    fn score(&self) -> u32 {
-        let matching_numbers = self
-            .drawn_numbers
+    fn matching_numbers(&self) -> u32 {
+        self.drawn_numbers
             .iter()
             .filter(|n| self.winning_numbers.contains(n))
-            .count();
+            .count() as u32
+    }
 
-        (1 << matching_numbers) >> 1
+    fn score(&self) -> u32 {
+        (1 << self.matching_numbers()) >> 1
     }
 }
 
@@ -49,7 +68,7 @@ impl FromStr for Card {
 
     fn from_str(string: &str) -> Result<Self, Self::Err> {
         if let [card_id, numbers] = string.split(':').collect::<Vec<&str>>().as_slice() {
-            let _id = if let ["Card", id] = card_id
+            let id = if let ["Card", id] = card_id
                 .split(' ')
                 .filter(|s| !s.is_empty())
                 .collect::<Vec<&str>>()
@@ -79,7 +98,7 @@ impl FromStr for Card {
             };
 
             Ok(Card {
-                _id,
+                id,
                 winning_numbers,
                 drawn_numbers,
             })
@@ -92,6 +111,7 @@ impl FromStr for Card {
 #[cfg(test)]
 mod test {
     use super::*;
+    use indoc::indoc;
 
     #[test]
     fn test_card_score() {
@@ -105,5 +125,23 @@ mod test {
         ] {
             assert_eq!(expected_score, Card::from_str(card).unwrap().score());
         }
+    }
+
+    #[test]
+    fn test_expand_cards() {
+        let cards: Vec<Card> = indoc! {"
+            Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
+            Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19
+            Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1
+            Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83
+            Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36
+            Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11
+        "}
+        .lines()
+        .map(Card::from_str)
+        .collect::<Result<_, _>>()
+        .unwrap();
+
+        assert_eq!(30, expand_cards(&cards));
     }
 }
